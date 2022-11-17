7e8dd71f19bd434f5d700e6de64108c08ddc9e63
Ahsan <ahsan@Ahsans-MacBook-Pro.local>
2022-11-18 00:53:47 +0500

    movie-api

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from movies.views import GenreMoviesListView, GenreView, IMDBFanFavoriteMoviesView, IMDBOriginalMoviesView, MostPopularMoviesView, MovieView, MovieDetailView, RatingView, RecentReleasedMoviesView, ReviewView, SearchMovies, Top250MoviesView, TopBoxOfficeMoviesView, TopPickMoviesView, RecentUpcomingMoviesView, IMDBPrimeVideosView
+from movies.views import GenreMoviesListView, GenreView, IMDBFanFavoriteMoviesView, IMDBOriginalMoviesView, MostPopularMoviesView, MovieView, MovieDetailView, RatingView, RecentReleasedMoviesView, ReviewView, SearchMovies, Top250MoviesView, TopPickMoviesView, RecentUpcomingMoviesView, IMDBPrimeVideosView
 urlpatterns = [
     path('home-movies/', MovieView.as_view(), name='movie'),
     path('movie/<int:pk>', MovieDetailView.as_view(), name='movie-detail'),
@@ -15,7 +15,6 @@ urlpatterns = [
     path('recent-released/', RecentReleasedMoviesView.as_view(), name='movies-recent-released'),
     path('recent-upcoming/', RecentUpcomingMoviesView.as_view(), name='movies-upcoming'),
     path('recent-upcoming/', RecentUpcomingMoviesView.as_view(), name='movies-upcoming'),
-    path('top-box-office/', TopBoxOfficeMoviesView.as_view(), name='movies-top-box'),
     path('genre/', GenreView.as_view(), name='genre'),
     path('search/title/', GenreMoviesListView.as_view(), name='genre_list'),
     path('search-movies/', SearchMovies.as_view(), name='movies-search'),
